eb0ee7984034354c22cdc0ee668888dd58e1322c
shubham somavamshi <shubhamraopatil123@gmail.com>
2025-09-17 14:09:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-//your code here
+const seconds = document.querySelector(".hand.second-hand");
+const minutes = document.querySelector(".hand.min-hand");
+const hours = document.querySelector(".hand.hour-hand");
+
+function updateClock(){
+    const now = new Date();
+
+    const CurrSec = now.getSeconds();
+    const CurrMin = now.getMinutes();
+    const CurrHours = now.getHours();
+
+    let SecRotate = (360 / 60) * CurrSec;
+    let MinRotate = ((360/60) * CurrMin) + CurrSec;
+    let HourRotate = (360 / 12) * CurrHours  + CurrMin + 0.5;
+
+  seconds.style.transform = `rotate(${SecRotate}deg)`;
+  minutes.style.transform = `rotate(${MinRotate}deg)`;
+  hours.style.transform = `rotate(${HourRotate}deg)`;
+}
+updateClock()
+setInterval(updateClock, 1000); 
+
+
