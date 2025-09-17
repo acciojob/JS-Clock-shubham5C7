2fec52d8d86ee8ae978c7853a64d0872ad15918d
shubham somavamshi <shubhamraopatil123@gmail.com>
2025-09-17 14:22:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -9,9 +9,9 @@ function updateClock(){
     const CurrMin = now.getMinutes();
     const CurrHours = now.getHours();
 
-    let SecRotate = (360 / 60) * CurrSec;
-    let MinRotate = ((360/60) * CurrMin) + CurrSec;
-    let HourRotate = (30 * CurrHours + (CurrMin / 60) * 30) + 90;
+    let SecRotate = (6 * CurrSec) + 90;
+  let MinRotate = (6 * CurrMin) + 90;
+  let HourRotate = (30 * CurrHours + (CurrMin / 60) * 30) + 90;
 
   seconds.style.transform = `rotate(${SecRotate}deg)`;
   minutes.style.transform = `rotate(${MinRotate}deg)`;
